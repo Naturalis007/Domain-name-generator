d6731af3207fc627c3842365933fa53b1c168f3e
Naturalis007 <tamara.salasvergara@gmail.com>
2022-11-14 03:29:58 +0000

    añadí extensiones

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
-var pronoun = ['nosotrossomos','ustedesson','nuestrosamigosson', 'mis'];
+var pronom = ['nosotrossomos','ustedesson','nuestrosamigosson', 'mis'];
 var adj = ['grandes', 'enormes', 'mini' ];
-var noun = ['dibujantes.com','elefantes.net', 'cocineros.es'];
+var sustan = ['dibujantes','elefantes', 'cocineros'];
+var exten = [".com", ".cl", ".es", ".pe", ".net"];
 
-for (i=0; i<pronoun.length; i++){
-    for (k=0; k<adj.length; k++){
-        for (y=0; y<noun.length; y++){
-            console.log(pronoun[i]+adj[k]+noun[y])
+for (i=0; i<pronom.length; i++){
+    for (j=0; j<adj.length; j++){
+        for (k=0; k<sustan.length; k++){
+            for (l=0; l<exten.length; l++){
+                console.log(pronom[i]+adj[j]+sustan[k]+exten[l])
+            }
         }
     }
 }
